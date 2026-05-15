00bd6195972ecb49f70bc3f99bc3e19225f62393
rafael brandt balmant <balmantrafael@gmail.com>
2026-05-15 15:24:44 -0300

    update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import { container } from './container.js';
+import express from "express";
+import { container } from "./container.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,9 +7,16 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 
+// webhook (GET)
 app.get("/webhook", (req, res) => {
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
+
   return res.status(200).send(challenge);
+});
+
+// START SERVER
+app.listen(PORT, () => {
+  console.log(`server rodando na porta ${PORT}`);
 });
